9848cc269b8dfa679f0b7c63d1c8baf86564a0d9
Shuangyi <s9tong@edu.uwaterloo.ca>
2017-09-23 18:02:49 -0400

    Fix virtual

--- hjscript.hpp
+++ hjscript.hpp
@@ -596,25 +596,25 @@ namespace hjbase {
 
         protected:
 
-            std::pair<int, bool> More_On_Command_Level_1(const const_itVecStr& command);
+            virtual std::pair<int, bool> More_On_Command_Level_1(const const_itVecStr& command);
 
-            std::pair<int, bool> More_On_Builtin_Function_Search_Level_1(const std::string& op);
+            virtual std::pair<int, bool> More_On_Builtin_Function_Search_Level_1(const std::string& op);
 
-            std::string More_On_Builtin_Function_Level_1(const std::string& op, int op_key, const const_itVecStr& vals);
+            virtual std::string More_On_Builtin_Function_Level_1(const std::string& op, int op_key, const const_itVecStr& vals);
 
-            std::pair<std::string, bool> More_On_Names_Query_Level_1(const std::string& name);
+            virtual std::pair<std::string, bool> More_On_Names_Query_Level_1(const std::string& name);
 
-            std::pair<std::string, bool> More_On_Slice_Operator_Level_1(const const_itVecStr& vals);
+            virtual std::pair<std::string, bool> More_On_Slice_Operator_Level_1(const const_itVecStr& vals);
 
-            void More_Cleanup_Level_1();
+            virtual void More_Cleanup_Level_1();
 
-            void More_On_Config_Level_1(const std::string& item, const std::string& status);
+            virtual void More_On_Config_Level_1(const std::string& item, const std::string& status);
 
-            void More_On_Info_Level_1(const const_itVecStr& command);
+            virtual void More_On_Info_Level_1(const const_itVecStr& command);
 
-            std::pair<bool, int> More_On_Check_If_GC_Required_Level_1(const std::string&  ref_val);
+            virtual std::pair<bool, int> More_On_Check_If_GC_Required_Level_1(const std::string&  ref_val);
 
-            void More_On_Delete_Object_Level_1(const std::string& ref_val, int type_code);
+            virtual void More_On_Delete_Object_Level_1(const std::string& ref_val, int type_code);
             
             std::string Evaluate_Expression(const const_itVecStr& expr);
             
